e3c4199090b5a351b5cb70c43ae19500362ddb98
ADAN ISMAEL SANTIAGO ANAYA <20161341@itoaxaca.edu.mx>
2024-11-16 20:40:38 -0600

    Cambios FinalesFinales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },  // Asegúrate de que esta ruta esté correctamente definida
+  { path: '', component: LoginComponent },  
   { path: 'home', component: HomeComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' }  // Redirige a la página de login si la ruta no existe
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
